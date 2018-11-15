4d5ecab90794fe25797915e1e4a6e4ca47fb4d39
Jitendra Thethi <jitendra.thethi@aricent.com>
2018-11-15 21:53:58 +0530

    show grouped by tags and the count passed / total

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Teakozi Log Viewer' });
 });
 
+function summary(test){
+  var t = {}
+  t.name = test.name;
+  t.valid = test.valid;
+  return t;
+}
+
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  res.render('report', { log:JSON.parse(req.body.log) });
+  var log = JSON.parse(req.body.log)
+  log.all_tags = {}
+  log.tests.forEach(t=>{
+    t.tags.split(",").forEach(tg=>{
+      tg = tg.trim();
+      if(log.all_tags[tg]==undefined) log.all_tags[tg]=[];
+      log.all_tags[tg].push(summary(t));
+    })
+  })
+
+  res.render('report', { log: log });
 });
 
 module.exports = router;
